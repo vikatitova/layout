d8971e026c51eeb7e50ce1161d420531b74c79e2
vikatitova <vikatitovaa@gmail.com>
2020-03-10 12:06:59 +0300

    finish

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
-$(document).ready(function() {
-    $(".burger-nav").on("click", function() {
-      $("header nav ul ul").toggleClass("open");
+window.addEventListener('scroll', () => {
+  if (pageYOffset >= (document.body.scrollHeight / 3)) {
+    arrowtotopspan.classList.add('startappear');
+
+    setTimeout(() => {
+      arrowtotopspan.classList.add('appearall');
     });
+  } else {
+    arrowtotopspan.classList.remove('appearall');
+
+    setTimeout(() => {
+      arrowtotopspan.classList.add('startappear');
+    });
+  }
 });
+
+
+
+const elem = document.getElementById('arrowtotopspan');
+elem.addEventListener( "click",(e) => {
+  e.preventDefault();
+  scrollToAnchor();
+});
+
+const scrollToAnchor = () => {
+  const elem = document.getElementsByTagName('html,body');
+  window.scrollTo({
+    top: elem.offsetTop-20, 
+    behavior: "smooth"
+  });
+}
+
+
+  document.querySelector(".burgerspan").addEventListener("click", ()=>{
+    document.querySelector("header").classList.add("show");
+    document.querySelector("main").classList.add("darker-main");
+    document.querySelector("#dark").classList.add("darker-span");
+    document.querySelector(".burgerspan").style.cssText = `z-index: 99`;
+  });
+  document.querySelector("#dark").addEventListener("click",()=>{
+    if(document.querySelector("#dark").classList.contains("darker-span")){
+    document.querySelector("header").classList.remove("show");
+  document.querySelector("main").classList.remove("darker-main");
+  document.querySelector("#dark").classList.remove("darker-span");
+  document.querySelector(".burgerspan").style.cssText = `z-index: 1000000`;
+}
+  });
+
